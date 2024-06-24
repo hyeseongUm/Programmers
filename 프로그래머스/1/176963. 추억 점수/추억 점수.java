b125fc34e34d62505453ee755b76280a4a55f879
hyeseongUm <155358391+hyeseongUm@users.noreply.github.com>
2024-06-25 06:19:36 +0900

    [level 1] Title: 추억 점수, Time: 0.01 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+
+        for (int i = 0; i < photo.length; i++) {
+            int tot = 0;
+            for (int k = 0; k < photo[i].length; k++) {
+                for (int j = 0; j < name.length; j++) {
+                    if(photo[i][k].equals(name[j])){
+                       tot += yearning[j];
+                    }
+                }
+            }
+            answer[i] = tot;
+        }
+        return answer;
+    }
+}
